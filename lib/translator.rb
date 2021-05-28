a969b4b8d7cc3403b24dac1a99b74414b79a019c
Kyle Andruczk <kyle.andruczk@gmail.com>
2021-05-28 02:40:00 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,33 @@
 # require modules here
+require "pry"
+require "yaml"
 
-def load_library
+
+def load_library(file)
   # code goes here
+  lib = YAML.load_file(file)
+  
+  final_lib = lib.each_with_object({}) do |(key, val), fin_arr|
+    fin_arr[key] = { :english=>val[0], :japanese=>val[1] }
+  end 
+  final_lib
+
 end
 
 def get_japanese_emoticon
   # code goes here
 end
 
-def get_english_meaning
+def get_english_meaning(file, emote)
   # code goes here
+  
+  lib = load_library(file)
+  
+  lib.each do |key, val|
+
+    val.each do |inner_key, inner_val|
+      binding.pry
+    
+  end 
+  
 end
